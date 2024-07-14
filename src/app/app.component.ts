389181b236cf88f047c6a7ee78a992652772d24e
kakon-selise <kakon.mehedi@selise.ch>
2024-07-14 14:47:54 +0600

    feat: Add editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,37 +21,68 @@ export class AppComponent implements AfterViewInit {
 	constructor(private cdr: ChangeDetectorRef, private ngZone: NgZone) {}
 
 	ngAfterViewInit() {
-		// this.updateCkeditorElementData();
+		this.updateCkeditorElementData();
 
-		// window.addEventListener('message', (event) => {
-		// 	if (event.data && event.data.type === 'DATA_CHANGED') {
-		// 		this.ngZone.run(() => {
-		// 			this.ckeditorData = event.data.data;
-		// 			this.cdr.detectChanges();
-		// 		});
-		// 	}
-		// });
+		window.addEventListener('message', (event) => {
+			if (event.data && event.data.type === 'DATA_CHANGED') {
+				this.ngZone.run(() => {
+					this.ckeditorData = event.data.data;
+					console.log(event.data.data);
+					this.cdr.detectChanges();
+				});
+			}
+		});
 
-		// const ckeditorElement =
-		// 	this.ckeditorElementContainer.nativeElement.querySelector(
-		// 		'#ckeditor-element'
-		// 	);
-		// ckeditorElement.addEventListener('dataChange', (event: CustomEvent) => {
-		// 	console.log('Data changed in CKEditor:', event.detail);
-		// 	parent.postMessage(
-		// 		{ type: 'DATA_CHANGED', data: event.detail },
-		// 		'*'
-		// 	);
-		// });
+		const ckeditorElement =
+			this.ckeditorElementContainer.nativeElement.querySelector(
+				'#ckeditor-element'
+			);
+		ckeditorElement.addEventListener('dataChange', (event: CustomEvent) => {
+			console.log('Data changed in CKEditor:', event.detail);
+			parent.postMessage(
+				{ type: 'DATA_CHANGED', data: event.detail },
+				'*'
+			);
+		});
 	}
 
 	private updateCkeditorElementData() {
-		// const ckeditorElement =
-		// 	this.ckeditorElementContainer.nativeElement.querySelector(
-		// 		'#ckeditor-element'
-		// 	);
-		// if (ckeditorElement) {
-		// 	ckeditorElement.setAttribute('data', this.ckeditorData);
-		// }
+		const ckeditorElement =
+			this.ckeditorElementContainer.nativeElement.querySelector(
+				'#ckeditor-element'
+			);
+		if (ckeditorElement) {
+			ckeditorElement.setAttribute('data', this.ckeditorData);
+		}
 	}
 }
+
+
+/**
+ * Solution 1: Web component 
+ * Why it did not work ?
+ * Duplicate module error => Need to remove from the whole app
+ * 
+ * 
+ * Solution 2: Iframe
+ */
+
+
+/**
+ * Build the app by npm run build - build scrpt from package .json
+ * We use ngx-build-plus for single build
+ * otherwise amader delta te integrate korte gele webpack e config korte hoto
+ * When adding to delta
+ * Angular.json er build er assests[] array te script gula load koray dite hobe
+ * 
+ * static file er path change kore dite hobe index.html er
+ */
+
+
+// Add this on angular.json build assets array
+
+// {
+// 	"glob": "**/*",
+// 	"input": "static",
+// 	"output": "/static"
+//   }
--- src/app/ckeditor/ckeditor.component.ts
+++ src/app/ckeditor/ckeditor.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, EventEmitter, Input, Output } from '@angular/core';
 
 import {
 	DecoupledEditor,
@@ -69,6 +69,144 @@ import {
 	Undo,
 } from 'ckeditor5';
 
+const DEFAULT_EDITOR_CONFIG = {
+	placeholder: 'Type the content here',
+	plugins: [
+		Alignment,
+		Autoformat,
+		AutoImage,
+		AutoLink,
+		Base64UploadAdapter,
+		BlockQuote,
+		Bold,
+		Code,
+		CodeBlock,
+		DataFilter,
+		DataSchema,
+		Essentials,
+		FindAndReplace,
+		FontBackgroundColor,
+		FontColor,
+		FontFamily,
+		FontSize,
+		GeneralHtmlSupport,
+		Heading,
+		Highlight,
+		HorizontalLine,
+		HtmlComment,
+		HtmlEmbed,
+		Image,
+		ImageCaption,
+		ImageInsert,
+		ImageResize,
+		ImageStyle,
+		ImageToolbar,
+		ImageUpload,
+		Indent,
+		IndentBlock,
+		Italic,
+		Link,
+		LinkImage,
+		List,
+		ListProperties,
+		MediaEmbed,
+		MediaEmbedToolbar,
+		Mention,
+		PageBreak,
+		Paragraph,
+		PasteFromOffice,
+		RemoveFormat,
+		SourceEditing,
+		SpecialCharacters,
+		SpecialCharactersArrows,
+		SpecialCharactersCurrency,
+		SpecialCharactersEssentials,
+		SpecialCharactersMathematical,
+		SpecialCharactersText,
+		StandardEditingMode,
+		Strikethrough,
+		Subscript,
+		Superscript,
+		Table,
+		TableCaption,
+		TableCellProperties,
+		TableProperties,
+		TableToolbar,
+		TextTransformation,
+		TodoList,
+		Underline,
+		WordCount,
+	],
+
+	toolbar: {
+		items: [
+			'undo',
+			'redo',
+			'heading',
+			'|',
+			'fontSize',
+			'fontFamily',
+			'|',
+			'fontColor',
+			'fontBackgroundColor',
+			'highlight',
+			'|',
+			'bold',
+			'italic',
+			'underline',
+			'strikethrough',
+			'superscript',
+			'subscript',
+			'removeFormat',
+			'findAndReplace',
+			'|',
+			'alignment',
+			'|',
+			'numberedList',
+			'bulletedList',
+			'todoList',
+			'|',
+			'outdent',
+			'indent',
+			'|',
+			'imageUpload',
+			'imageInsert',
+			'insertTable',
+			'mediaEmbed',
+			'|',
+			'horizontalLine',
+			'link',
+			'blockQuote',
+			'code',
+			'codeBlock',
+			'htmlEmbed',
+			'pageBreak',
+			'specialCharacters',
+			//'sourceEditing',
+			//'restrictedEditingException'
+		],
+		shouldNotGroupWhenFull: true,
+	},
+	language: 'en',
+	image: {
+		toolbar: [
+			'imageTextAlternative',
+			'imageStyle:inline',
+			'imageStyle:block',
+			'imageStyle:side',
+			'linkImage',
+		],
+	},
+	table: {
+		contentToolbar: [
+			'tableColumn',
+			'tableRow',
+			'mergeTableCells',
+			'tableCellProperties',
+			'tableProperties',
+		],
+	},
+};
 @Component({
 	selector: 'app-ckeditor-delta',
 	templateUrl: './ckeditor.component.html',
@@ -76,146 +214,25 @@ import {
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CkeditorComponent {
+	@Input()
+	hasCustomEditorConfig: boolean = false;
+
+	@Input()
+	editorConfig: any = DEFAULT_EDITOR_CONFIG;
+
+	@Input()
+	focusOnStart: boolean = true;
+
+	@Output()
+	blur: EventEmitter<any> = new EventEmitter();
+
+	@Output()
+	change: EventEmitter<any> = new EventEmitter();
+
 	ckEditorCssFileLocation = 'ck-editor.style.css';
 	editorInstance: any;
-	editorConfig = {
-		placeholder: 'Type the content here',
-		plugins: [
-			Alignment,
-			Autoformat,
-			AutoImage,
-			AutoLink,
-			Base64UploadAdapter,
-			BlockQuote,
-			Bold,
-			Code,
-			CodeBlock,
-			DataFilter,
-			DataSchema,
-			Essentials,
-			FindAndReplace,
-			FontBackgroundColor,
-			FontColor,
-			FontFamily,
-			FontSize,
-			GeneralHtmlSupport,
-			Heading,
-			Highlight,
-			HorizontalLine,
-			HtmlComment,
-			HtmlEmbed,
-			Image,
-			ImageCaption,
-			ImageInsert,
-			ImageResize,
-			ImageStyle,
-			ImageToolbar,
-			ImageUpload,
-			Indent,
-			IndentBlock,
-			Italic,
-			Link,
-			LinkImage,
-			List,
-			ListProperties,
-			MediaEmbed,
-			MediaEmbedToolbar,
-			Mention,
-			PageBreak,
-			Paragraph,
-			PasteFromOffice,
-			RemoveFormat,
-			SourceEditing,
-			SpecialCharacters,
-			SpecialCharactersArrows,
-			SpecialCharactersCurrency,
-			SpecialCharactersEssentials,
-			SpecialCharactersMathematical,
-			SpecialCharactersText,
-			StandardEditingMode,
-			Strikethrough,
-			Subscript,
-			Superscript,
-			Table,
-			TableCaption,
-			TableCellProperties,
-			TableProperties,
-			TableToolbar,
-			TextTransformation,
-			TodoList,
-			Underline,
-			WordCount,
-		],
+	data: any = '';
 
-		toolbar: {
-			items: [
-				'undo',
-				'redo',
-				'heading',
-				'|',
-				'fontSize',
-				'fontFamily',
-				'|',
-				'fontColor',
-				'fontBackgroundColor',
-				'highlight',
-				'|',
-				'bold',
-				'italic',
-				'underline',
-				'strikethrough',
-				'superscript',
-				'subscript',
-				'removeFormat',
-				'findAndReplace',
-				'|',
-				'alignment',
-				'|',
-				'numberedList',
-				'bulletedList',
-				'todoList',
-				'|',
-				'outdent',
-				'indent',
-				'|',
-				'imageUpload',
-				'imageInsert',
-				'insertTable',
-				'mediaEmbed',
-				'|',
-				'horizontalLine',
-				'link',
-				'blockQuote',
-				'code',
-				'codeBlock',
-				'htmlEmbed',
-				'pageBreak',
-				'specialCharacters',
-				//'sourceEditing',
-				//'restrictedEditingException'
-			],
-			shouldNotGroupWhenFull: true,
-		},
-		language: 'en',
-		image: {
-			toolbar: [
-				'imageTextAlternative',
-				'imageStyle:inline',
-				'imageStyle:block',
-				'imageStyle:side',
-				'linkImage',
-			],
-		},
-		table: {
-			contentToolbar: [
-				'tableColumn',
-				'tableRow',
-				'mergeTableCells',
-				'tableCellProperties',
-				'tableProperties',
-			],
-		},
-	};
 	ngAfterViewInit(): void {
 		this.loadStyles();
 		this.loadCKEditor();
@@ -256,23 +273,33 @@ export class CkeditorComponent {
 
 		const availablePlugins = editor.plugins;
 
-		console.log('Available Plugins:', availablePlugins);
+		// console.log('Available Plugins:', availablePlugins);
 
+		if (this.hasCustomEditorConfig) {
+			this.setCustomEditorConfig(editor, this.editorConfig);
+		}
+	}
+
+	emitOutputValueEvents() {
+		this.emitOnChangeOutputData();
+		this.emitOnBlurOutputData();
+	}
+
+	emitOnChangeOutputData() {
 		this.editorInstance.model.document.on('change:data', () => {
 			const data = this.editorInstance.getData();
-			console.log('Content has changed:', data);
+			this.change.emit(data);
 		});
 	}
-	getEditorContent(): void {
-		if (this.editorInstance) {
-			const content = this.editorInstance.getData();
-			console.log('Current content:', content);
-		}
+
+	emitOnBlurOutputData() {
+		this.editorInstance.ui.focusTracker.on('change:isFocused', () => {
+			const data = this.editorInstance.getData();
+			this.blur.emit(data);
+		});
 	}
 
-	setEditorContent(content: string): void {
-		if (this.editorInstance) {
-			this.editorInstance.setData(content);
-		}
+	setCustomEditorConfig(editor: any, customConfig: any) {
+		editor.config.set(customConfig);
 	}
 }
